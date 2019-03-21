ef8ad0e747ce181e9398fb5a0e7682e633c1ff25
giciarz412 <tomaszfefler1990@gmail.com>
2019-03-21 18:44:06 +0100

    spring standalone application

--- src/main/java/com/example/demo/topic/TopicController.java
+++ src/main/java/com/example/demo/topic/TopicController.java
@@ -3,14 +3,25 @@ package com.example.demo.topic;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicController {
 
+
+    // Singleton
+    //
+    private TopicService topicService;
+
     @RequestMapping("/topics")
-    public List<Topic> getAllTopics(){
-        return "All Topics";
+    public List<Topic> getAllTopics() {
+        return Arrays.asList(
+                new Topic("spring", "Spring Framework", "Spring Framework Description"),
+                new Topic("java", "Core java", "Core Java Description"),
+                new Topic("JavaScript", "JavaScript ", "JavaScript Description")
+
+        );
     }
 
 
---
+++ src/main/java/com/example/demo/topic/TopicService.java
@@ -0,0 +1,7 @@
+package com.example.demo.topic;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+}

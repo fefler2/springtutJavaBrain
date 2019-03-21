2cf0e6321177c621e8bfb22ba1e4623d12a666f8
giciarz412 <tomaszfefler1990@gmail.com>
2019-03-21 17:55:09 +0100

    spring standalone application

--- src/main/java/com/example/demo/topic/TopicController.java
+++ src/main/java/com/example/demo/topic/TopicController.java
@@ -3,11 +3,13 @@ package com.example.demo.topic;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TopicController {
 
     @RequestMapping("/topics")
-    public String getAllTopics(){
+    public List<Topic> getAllTopics(){
         return "All Topics";
     }
 
